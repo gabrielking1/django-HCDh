ca4c3bb4338a1b01b7c4eb19a616bab08b467a98
gabrielking1 <gkgabrielking@gmail.com>
2023-05-04 10:59:09 +0100

    woah update

--- mysite/settings.py
+++ mysite/settings.py
@@ -162,7 +162,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'django-hcdh-production.up.railway.app','static')
+    os.path.join(BASE_DIR, 'static')
 ]
     
 # Media Folder Settings
